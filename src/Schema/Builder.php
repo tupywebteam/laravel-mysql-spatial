213f851fe3692890a26fcf73f5dcf203ed07b12e
tupywebteam <50232450+tupywebteam@users.noreply.github.com>
2025-05-22 12:04:40 -0300

    Update Builder.php

--- src/Schema/Builder.php
+++ src/Schema/Builder.php
@@ -15,8 +15,8 @@ class Builder extends MySqlBuilder
      *
      * @return Blueprint
      */
-    protected function createBlueprint($table, Closure $callback = null)
-    {
-        return new Blueprint($table, $callback);
-    }
+    //protected function createBlueprint($table, Closure $callback = null)
+    //{
+    //    return new Blueprint($table, $callback);
+    //}
 }
